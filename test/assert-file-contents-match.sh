94562b299c29ac51935a78622b669e8d8b59da3d
Danielle Adams <danielle.adams3@extendhealth.com>
2024-03-06 14:25:52 -0700

    ARCH-2011 - Adding status check tests

--- test/assert-file-contents-match.sh
+++ test/assert-file-contents-match.sh
@@ -27,32 +27,33 @@ done
 
 echo "
 Asserting file contents match:
-Expected file name: '$expectedFileName'
-Actual file name:   '$actualFileName'"
+File with expected contents: '$expectedFileName'
+File with actual contents:   '$actualFileName'"
 
 # First make sure the actual file exists
 if [ -f "$actualFileName" ]
 then
-  echo "
-$actualFileName exists which is expected."
+  echo "The file with actual contents exists which is expected."
   actualFileContents=$(cat $actualFileName)
 else
-  echo "
-$actualFileName does not exist which is not expected"
+  echo "The file with actual contents does not exist which is not expected"
   exit 1
 fi
 expectedFileContents=$(cat $expectedFileName)
 
+# Then print the contents
+echo "::group::Expected file contents"
+echo "$expectedFileContents"
+echo "::endgroup::"
 
-# Then compare the contents
-name="file contents"
-echo "
-Expected $name: '$expectedFileContents'
-Actual $name:   '$actualFileContents'"
+echo "::group::Actual file contents"
+echo "$actualFileContents"
+echo "::endgroup::"
 
+# And finally compare the contents
 if [ "$expectedFileContents" != "$actualFileContents" ]; then
-  echo "The expected $name does not match the actual $name."  
+  echo "The expected contents do not match the actual contents, which is not expected."  
   exit 1
 else 
-  echo "The expected and actual $name values match."
+  echo "The expected and actual content values match, which is expected."
 fi
