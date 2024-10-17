4fd7aa6c0a5e9fe4a196a76d6817033409ff1a9d
Georg Friedrich <GFriedrich@users.noreply.github.com>
2024-10-17 20:34:35 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -223,7 +223,7 @@ function getIndividualCoverageInfo(summaryInputData, coverageType, threshold, ig
   }
 
   const itemFound = summaryInputData.match(regex);
-  infoToReturn.actualCoverage = itemFound && itemFound[1] ? parseInt(itemFound[1]) : 0;
+  infoToReturn.actualCoverage = itemFound && itemFound[1] ? parseFloat(itemFound[1]) : 0;
   if (infoToReturn.threshold === 0) {
     infoToReturn.conclusion = 'neutral';
   } else {
